e71198142ebf3cb21e6caeaecb592b6da0f158c7
SUZY <victoria1230120@gmail.com>
2025-01-24 11:07:46 +0900

    타입 B 메인PC 퍼블

--- js/slide.js
+++ js/slide.js
@@ -13,10 +13,22 @@ $(document).ready(function(){
         nextArrow : $('.nextBtn'), 
     });
 
-// 중앙 슬라이드에 효과를 적용하기 위해 slick-center 클래스를 사용
-$('.main_4_slick_wrap').on('afterChange', function (event, slick, currentSlide) {
-    $('.main_4_slick').removeClass('active-slide'); // 이전 active-slide 제거
-    $('.main_4_slick.slick-center').addClass('active-slide'); // 중앙 슬라이드에 추가
-});
+    // 중앙 슬라이드에 효과를 적용하기 위해 slick-center 클래스를 사용
+    $('.main_4_slick_wrap').on('afterChange', function (event, slick, currentSlide) {
+        $('.main_4_slick').removeClass('active-slide'); // 이전 active-slide 제거
+        $('.main_4_slick.slick-center').addClass('active-slide'); // 중앙 슬라이드에 추가
+    });
+
+    /* ---------------------------------------------------- */
+    /* 포토갤러리 슬라이드 */
+    $('.photo_slideWrap').slick({
+        infinite: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+
 
 });
