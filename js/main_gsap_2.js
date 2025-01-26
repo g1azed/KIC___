2bef9daee90b5756d9589f8445508dd4f5e587b4
g1azed <victoria1230120@gmail.com>
2025-01-26 20:13:39 +0900

    main 백업

---
+++ js/main_gsap_2.js
@@ -0,0 +1,102 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// scroll smooth
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
+
+
+
+
+// 스크롤따라 img 커지면서 올라오기
+gsap.set(".sec_establish_bg", { width: "85vw", height: "85vh", y: -600 });
+gsap.set(".sec_esta_text", {y: 900, opacity: 0,});
+gsap.to(".sec_establish_bg", {
+    height: "100%",
+    width: "100%",
+    ease: "none",
+    y: 0,
+    scrollTrigger: {
+        trigger: ".sec_establish_bg",
+        start: "top center",
+        end: "+=700",
+        scrub: 1,
+        // markers: {
+        //     startColor:'red',
+        //     endColor: 'red',
+        // },
+        onLeave: () => {
+            console.log("백그라운드 이미지 끝")
+            gsap.to(".sec_esta_text", {
+                y: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    // markers: {
+                    //     startColor:'blue',
+                    //     endColor: 'blue',
+                    // },
+                    trigger: ".sec_esta_text",
+                    start: "-1000px",
+                    end: "+=100",
+                    scrub: 8,
+                },
+            });
+        }
+    },
+});
+
+
+/* 섹션 레이어드 */
+gsap.set(".layerd_1", { width: "60%", height: "60%", y: 500 });
+gsap.to(".layerd_1", {
+    height: "100%",
+    width: "100%",
+    ease: "none",
+    y: 0,
+    scrollTrigger: {
+        markers: {
+            startColor: 'white',
+            endColor: 'white',
+        },
+        trigger: ".scroll_card_1",
+        start: '20% center',
+        end: '+=900',
+        scrub: 1,
+        // onleave / leave enter
+    }
+})
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: true,
+    }
+})
+  .to(".box", { scale: 2 }) // 커짐
+  .to(".box", { scale: 1 }); // 다시 작아짐
+
+
+// gsap.utils.toArray(".layerd").forEach((panel, i) => {
+//     ScrollTrigger.create({
+//         trigger: panel,
+//         start: "top center",
+//         // end: () => "+=" + (window.innerHeight * 2 - 4),
+//         end: 100,
+//     });
+// });
+
+// gsap.utils.toArray(".main_flex_secWrap").forEach((panel, i) => {
+//     ScrollTrigger.create({
+//         trigger: panel,
+//         start: "bottom bottom",
+//         end: () => "+=" + (window.innerHeight * 2 - 4),
+//     });
+// });
+
+
+/* 마우스 커서 */
+// 
