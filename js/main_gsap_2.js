061bab8980ffd1f27c9a3b573bf6639904fd6b64
SUZY <victoria1230120@gmail.com>
2025-01-27 03:44:52 +0900

    1111

--- js/main_gsap_2.js
+++ js/main_gsap_2.js
@@ -50,34 +50,37 @@ gsap.to(".sec_establish_bg", {
 
 
 /* 섹션 레이어드 */
-gsap.set(".layerd_1", { width: "60%", height: "60%", y: 500 });
-gsap.to(".layerd_1", {
-    height: "100%",
-    width: "100%",
-    ease: "none",
-    y: 0,
+   // 박스가 커지는 애니메이션
+   gsap.to(".layerd_1", {
+    width: 300, // 너비를 300px로 증가
+    height: 300, // 높이를 300px로 증가
     scrollTrigger: {
-        markers: {
-            startColor: 'white',
-            endColor: 'white',
-        },
-        trigger: ".scroll_card_1",
-        start: '20% center',
-        end: '+=900',
-        scrub: 1,
-        // onleave / leave enter
-    }
-})
-gsap.timeline({
+      trigger: ".layerd_1", // 트리거 대상
+      start: "top 80%", // 애니메이션 시작 위치
+      end: "top 50%", // 애니메이션 종료 위치
+      scrub: true, // 스크롤과 애니메이션 동기화
+    },
+  });
+
+  // 커진 상태 유지 및 스크롤 고정
+  ScrollTrigger.create({
+    trigger: ".layerd_1",
+    start: "top 50%", // 고정 시작
+    end: "+=300", // 고정 유지 구간 (300px 스크롤 동안)
+    pin: true, // 박스를 고정
+  });
+
+  // 박스가 다시 작아지는 애니메이션
+  gsap.to(".layerd_1", {
+    width: 100, // 너비를 100px로 축소
+    height: 100, // 높이를 100px로 축소
     scrollTrigger: {
-        trigger: ".box",
-        start: "top 80%",
-        end: "top 20%",
-        scrub: true,
-    }
-})
-  .to(".box", { scale: 2 }) // 커짐
-  .to(".box", { scale: 1 }); // 다시 작아짐
+      trigger: ".layerd_1",
+      start: "top 50%+=300", // 고정 구간이 끝난 후 시작
+      end: "top -20%", // 애니메이션 종료 위치
+      scrub: true, // 스크롤과 애니메이션 동기화
+    },
+  });
 
 
 // gsap.utils.toArray(".layerd").forEach((panel, i) => {
