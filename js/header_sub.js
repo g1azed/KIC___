296dc82c0c95d5c1c7a47d7b0e465df3f9cb53d0
SUZY <victoria1230120@gmail.com>
2025-02-02 08:55:01 +0900

    sub_01 헤더 수정

--- js/header_sub.js
+++ js/header_sub.js
@@ -1,29 +1,26 @@
 $(document).ready(function () {
     var prevScrollTop = 0;
     $(document).on('scroll', function(){
+        var nowScrollTop = $(window).scrollTop(); //현재 스크롤 위치를 nowScrollTop 에 저장
 
-        if($(window).scrollTop() >= 1000){
-            var nowScrollTop = $(window).scrollTop(); //현재 스크롤 위치를 nowScrollTop 에 저장
-        
-            if (nowScrollTop > prevScrollTop){ 
-                $('header').addClass('active'); 
-            }  else {
-                $('header').removeClass('active'); 
-            } // 스크롤 방향(Up) 올릴때 -> 헤더에 active 클래스 제거
-
-
-            prevScrollTop = nowScrollTop;  // prevScroll, nowScrollTop 조건 판단 후, 현재 스크롤값을 prevScrollTop 에 저장
+        if (nowScrollTop > prevScrollTop){ 
+            $('header').addClass('active'); 
+        }  else {
+            $('header').removeClass('active'); 
+        } // 스크롤 방향(Up) 올릴때 -> 헤더에 active 클래스 제거
     
+    
+        prevScrollTop = nowScrollTop;
+
+        if( $(window).scrollTop() >= 900 &&  $(window).scrollTop() < 1800 ){ 
+
             $("#header").css('background-color','transparent')
             $(".header_content ul li a").css('color','#ffffff')
             // $('.header_logo_img').attr('src', 'https://g1azed.github.io/KIC/assets/logo/KIC_LOGO_White.svg')
             $('.header_logo_img').attr('src', './assets/logo/KIC_LOGO_White.svg')
             $('.icon_search').attr('src', './assets/icon/KIC_search_White.svg')
             $('.icon_lang').attr('src', './assets/icon/KIC_Language_icon_White.svg')
-    
-        }else{ 
-
-
+        }else{
             $("#header").css('background-color','#ffffff')
             $(".header_content ul li a").css('color','#121212')
             // $('.header_logo_img').attr('src', 'https://g1azed.github.io/KIC/assets/logo/KIC_LOGO_Blue.svg')
@@ -33,3 +30,4 @@ $(document).ready(function () {
         }
     })
 });
+
